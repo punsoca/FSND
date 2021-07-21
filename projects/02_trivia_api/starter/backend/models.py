c859f3fb43837123b5ca314428470ba3bd744b81
punsoca <punsoca@gmail.com>
2021-07-21 13:01:31 -0700

    changes made for second review

--- projects/02_trivia_api/starter/backend/models.py
+++ projects/02_trivia_api/starter/backend/models.py
@@ -3,8 +3,11 @@ from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "trivia"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+from os import environ as env
+from dotenv import load_dotenv
+
+load_dotenv()
+database_path = "postgres://{}:{}@{}/{}".format(env['DB_USER'], env['DB_PASSWORD'], env['DB_HOST'], env['DB_NAME'])
 
 db = SQLAlchemy()
 
--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -6,6 +6,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flaskr import create_app
 from models import setup_db, Question, Category
 
+from os import environ as env
+from dotenv import load_dotenv
+
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
@@ -14,9 +17,7 @@ class TriviaTestCase(unittest.TestCase):
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
-        self.database_name = "trivia_test"
-        # self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
-        self.database_path = "postgres://{}:{}@{}/{}".format('student', 'student','localhost:5432', self.database_name)
+        self.db_path = "postgres://{}:{}@{}/{}".format(env['DB_USER'], env['DB_PASSWORD'], env['DB_HOST'], env['TEST_DB_NAME'])
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
