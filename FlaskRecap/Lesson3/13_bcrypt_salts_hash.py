50cb7e83b70298eeadd3ae8522fed50ad88980db
punsoca <punsoca@gmail.com>
2021-07-24 15:26:54 -0700

    add Lesson3

---
+++ FlaskRecap/Lesson3/13_bcrypt_salts_hash.py
@@ -0,0 +1,27 @@
+# NOTE: this program does not use Flask
+
+import bcrypt
+
+# refer to bcrypt package documentation on https://github.com/pyca/bcrypt/
+
+# note that passwords need to be in converted to binary format before getting hashed
+password = b"studyhard"
+
+# Hash a password for the first time, with a certain number of rounds - bycrpt.gensalt() allows you to specify the number
+# The higher the round number the better; but in practice, the optimal number depends on machine's processor (google for further info)
+salt = bcrypt.gensalt(14)
+hashed = bcrypt.hashpw(password, salt)
+# print them to see how the values look like
+print(salt, hashed)   
+
+
+# use bcrypt.checkpw(password,hashed_value) to verify the password
+# here is a little exercise to see which one from a set of passwords matches a given hash
+hash_value = b'$2b$14$EFOxm3q8UWH8ZzK1h.WTZeRcPyr8/X0vRfuL3/e9z7AKIMnocurBG'
+# again, note that passwords need to be cast from strings to bytes for bcrypt to work
+passwords = [b'securepassword', b'udacity', b'learningisfun']
+# run to see which password in the list matches the hash_value
+for password in passwords:
+    if bcrypt.checkpw(password, hash_value):
+        print(f'The password {password} matches the hashed value {hash_value}')
+        break
---
+++ FlaskRecap/Lesson3/8_test_cryptography.py
@@ -0,0 +1,23 @@
+# Import Package
+
+# if  cryptography lib not present, run 'pip3 install cryptography'
+from cryptography.fernet import Fernet
+
+# Generate a Key and Instantiate a Fernet Instance
+# key = Fernet.generate_key()
+key= b'8cozhW9kSi5poZ6TWFuMCV123zg-9NORTs3gJq_J5Do='
+f = Fernet(key)
+print(key)
+
+# Define our message
+plaintext = b"encryption is very useful"
+plaintext = b'encrypting is just as useful'
+
+# Encrypt
+ciphertext = f.encrypt(plaintext)
+print(ciphertext)
+
+# Decrypt
+ciphertext = b'gAAAAABc8Wf3rxaime-363wbhCaIe1FoZUdnFeIXX_Nh9qKSDkpBFPqK8L2HbkM8NCQAxY8yOWbjxzMC4b5uCaeEpqDYCRNIhnqTK8jfzFYfPdozf7NPvGzNBwuuvIxK5NZYJbxQwfK72BNrZCKpfp6frL8m8pdgYbLNFcy6jCJBXATR3gHBb0Y='
+decryptedtext = f.decrypt(ciphertext)
+print(decryptedtext)
